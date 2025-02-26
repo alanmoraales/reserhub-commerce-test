ebad855200cb5083b6f81584b246088b92a8b00b
alanmoraales <alanmoraales845@gmail.com>
2025-02-26 14:02:21 -0600

    make encore.dev a dependency

--- apps/commerce-api/hello/hello.ts
+++ apps/commerce-api/hello/hello.ts
@@ -15,7 +15,7 @@ import { api } from "encore.dev/api";
 export const get = api(
   { expose: true, method: "GET", path: "/hello/:name" },
   async ({ name }: { name: string }): Promise<Response> => {
-    const msg = `Hello ${name}! sssssss`;
+    const msg = `Hello ${name}!`;
     return { message: msg };
   }
 );
